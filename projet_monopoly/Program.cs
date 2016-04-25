d65b3851ba824ff699bb209bef04323a8348ac91
Juliette Pelletier <jpelletie001@ensc.fr>
2016-04-25 15:34:49 +0200

    first commit

---
+++ projet_monopoly/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projet_monopoly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tototototo
+        }
+    }
+}
